9d7fe9e1769360985848163725128fb8ed4fb74b
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-03 20:43:04 +0900

    ✨ [SCRUM-79]-main-Card-컴포넌트-제작

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -1,6 +1,7 @@
 import { Logo } from "@/components/common/Logo";
 import { Header } from "@/components/shared/Header";
 import TodoItem from "@/components/shared/TodoItem";
+import Card from "@/components/common/Card"
 
 export const Home = () => {
   return (
@@ -9,7 +10,12 @@ export const Home = () => {
       <Header>
         <Logo width={45} height={45} />
       </Header>
-      <TodoItem category="영어" text="영어 단어 20개 외우기" />
+
+       <div className="mt-6 flex flex-col items-center gap-4">
+        <Card title="목표 진행률" isMore height="h-40" />
+        <TodoItem category="영어" text="영어 단어 20개 외우기" />
+      </div>
+      
     </div>
   );
 };
