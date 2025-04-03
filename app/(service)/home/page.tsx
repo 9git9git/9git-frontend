b5f4b4499ecaad700b0a4397a1b91d93a5d7028e
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-03 13:52:29 +0900

    ✨ [SCRUM-79]-main-card-컴포넌트-제작

---
+++ app/(service)/home/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/components/common/Card"
+
+export default function Home() {
+  return (
+      <div className="flex flex-col items-center pt-8 space-y-6">
+    <Card title="오늘의 목표 진행률" height="h-40" />
+</div>
+  );
+}
---
+++ components/common/Card.tsx
@@ -0,0 +1,29 @@
+// 목표 진행률
+import {Separator} from "@/components/ui/separator"
+import { ReactNode } from "react"
+
+interface CardProps {
+  title: string
+  height?: string
+  more?: ReactNode
+}
+
+export default function Card({
+  title,
+  height, //높이 수정
+  more = (
+    <div className="text-sm text-[#7F4E28] font-semibold hover:text-[#A35F30]">
+      + 더보기
+    </div>
+  ),
+}: CardProps) {
+  return (
+    <section className={`bg-[#fff0d5] rounded-xl p-4 space-y-2 w-[90%] ${height}`}>
+      <div className="flex justify-between items-center font-semibold">
+        <span>{title}</span>
+        {more}
+      </div>
+      <Separator className="bg-[#f4ddbd]" />
+    </section>
+  )
+}
