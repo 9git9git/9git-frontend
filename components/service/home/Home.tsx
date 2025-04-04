67fd8b0caf6b1e94a6d7a94b26f7a0ca3adaaa6a
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-04 13:09:47 +0900

    progressbar 수정

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -2,6 +2,7 @@ import { Logo } from '@/components/common/Logo';
 import { Header } from '@/components/shared/Header';
 import TodoItem from '@/components/shared/TodoItem';
 import Card from '@/components/common/Card';
+import { ProgressBar } from '@/components/service/home/ProgressBar';
 
 export const Home = () => {
   return (
@@ -11,7 +12,8 @@ export const Home = () => {
       </Header>
 
       <div className="mt-6 flex flex-col items-center gap-4">
-        <Card title="목표 진행률" isMore height="h-40">
+        <Card title="목표 진행률" isMore>
+          <ProgressBar value={75} title="영어" />
           <TodoItem category="영어" text="영어 단어 20개 외우기" />
         </Card>
       </div>
