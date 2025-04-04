20f7bb660b0bc66be8ac70f71cf29b6706dc719f
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-05 01:38:42 +0900

    TodoItem에 체크박스 자리 마련 및 상태 연결 준비

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -6,6 +6,7 @@ import Card from '@/components/common/Card';
 import { ProgressBar } from '@/components/service/home/ProgressBar';
 import { ActionButton } from '@/components/common/ActionButton';
 import { Plus } from 'lucide-react';
+import CustomBadge from '@/components/common/CustomBadge';
 
 export const Home = () => {
   return (
@@ -17,6 +18,7 @@ export const Home = () => {
       <div className="mt-6 flex flex-col items-center gap-4">
         <Card title="목표 진행률" isMore>
           <ProgressBar value={75} title="영어" />
+
           <TodoItem category="영어" text="영어 단어 20개 외우기" />
         </Card>
       </div>
