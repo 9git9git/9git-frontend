36021ecab0678ec090f42e8e29569ef083f250b9
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-04 23:55:11 +0900

    🎨 [SCRUM-83]-fe-완료-button-컴포넌트-제작

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -4,6 +4,7 @@ import { Header } from '@/components/shared/Header';
 import TodoItem from '@/components/shared/TodoItem';
 import Card from '@/components/common/Card';
 import { ProgressBar } from '@/components/service/home/ProgressBar';
+import CompleteButton from '@/components/common/CompleteButton';
 
 export const Home = () => {
   return (
@@ -16,6 +17,7 @@ export const Home = () => {
         <Card title="목표 진행률" isMore>
           <ProgressBar value={75} title="영어" />
           <TodoItem category="영어" text="영어 단어 20개 외우기" />
+          <CompleteButton onClick={() => console.log('클릭됨!')} />
         </Card>
       </div>
     </div>
