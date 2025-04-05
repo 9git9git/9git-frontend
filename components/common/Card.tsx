76f001a4e91193d950b3fdf077db89ec45eed53c
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-05 20:36:23 +0900

    ✨ SCRUM-101-fe-오늘의-to-do-팝업-제작

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -3,17 +3,11 @@ import { Separator } from '@/components/ui/separator';
 
 interface CardProps {
   title: string;
-  height?: string;
   isMore?: boolean;
   children?: React.ReactNode; //chilren 생성
 }
 
-export default function Card({
-  title,
-  height, //높이 수정
-  isMore = false,
-  children,
-}: CardProps) {
+export default function Card({ title, isMore = false, children }: CardProps) {
   return (
     <section className="bg-[#fff0d5] rounded-xl p-4 w-[90%] flex flex-col gap-y-4">
       <div className="flex justify-between items-center font-semibold">
