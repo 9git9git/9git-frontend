22ffce0e153dcb155362e4aa839118adfba98f6d
Kristyn Yoon <rhombic2@naver.com>
2025-04-06 01:01:55 +0900

    리뷰 반영

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -3,18 +3,24 @@
 import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { CheckIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 interface CheckboxProps extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
   checked?: boolean;
   onChange?: (checked: boolean) => void;
+  className?: string;
 }
 
-function Checkbox({ checked, onChange, ...props }: CheckboxProps) {
+function Checkbox({ checked, onChange, className, ...props }: CheckboxProps) {
   return (
     <CheckboxPrimitive.Root
+      data-slot="checkbox"
       checked={checked}
       onCheckedChange={onChange}
-      className="w-[18px] h-[18px] border-[2px] border-[#744D2C] rounded-[4px] data-[state=checked]:bg-[#FCAA2B] data-[state=checked]:border-[#FCAA2B]"
+      className={cn(
+        'w-[18px] h-[18px] border-[2px] border-[#744D2C] rounded-[4px] data-[state=checked]:bg-[#FCAA2B] data-[state=checked]:border-[#FCAA2B]',
+        className
+      )}
       {...props}
     >
       <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current transition-none">
