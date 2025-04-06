18afd50b55cc1ec5247647bfd16e7e9f338b1b35
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-06 10:49:34 +0900

    🎨 [SCRUM-101]-fe-오늘의-to-do-팝업-수정

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -3,8 +3,7 @@ import { Logo } from '@/components/common/Logo';
 import { Header } from '@/components/shared/Header';
 import Card from '@/components/common/Card';
 import { ProgressBar } from '@/components/service/home/ProgressBar';
-import { ActionButton } from '@/components/common/ActionButton';
-import { Plus } from 'lucide-react';
+
 import TodayTodo from './TodayToDo'; //
 
 export const Home = () => {
@@ -18,12 +17,6 @@ export const Home = () => {
         {/* 목표 진행률 카드 */}
         <Card title="목표 진행률" isMore>
           <ProgressBar value={75} title="영어" />
-          {/* <ActionButton onClick={() => console.log('추가')} icon={<Plus size={16} />}>
-            추가
-          </ActionButton>
-          <ActionButton onClick={() => console.log('완료')}>완료</ActionButton>
-
-          <ActionButton onClick={() => console.log('삭제')}>삭제</ActionButton> */}
         </Card>
 
         {/* 오늘의 할 일 카드 */}
