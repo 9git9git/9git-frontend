b486578f8f2393b4d7ea2e0d98bdff017a2bf88f
Kristyn Yoon <rhombic2@naver.com>
2025-04-08 15:06:37 +0900

    ✨ [SCRUM-115] FE radio 컴포넌트 제작

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "구깃",
-  description: "필요한 순간만 접어놓다, 내 하루의 구깃",
+  title: '구깃',
+  description: '필요한 순간만 접어놓다, 내 하루의 구깃',
 };
 
 export default function RootLayout({
--- components/service/profile/ProfileModal/ProfileModal.tsx
+++ components/service/profile/ProfileModal/ProfileModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { GenderRadio } from '@/components/service/profile/ProfileModal/GenderRadio';
 import JobSelectbar from '@/components/service/profile/ProfileModal/JobSelectbar';
 import { Separator } from '@/components/ui/separator';
 import { TextScroll } from '@/components/service/profile/ProfileModal/TextScroll';
@@ -8,7 +9,7 @@ import { PERSONAL_INFORMATION_AGREEMENT } from '@/constants/PERSONAL_INFORMATION
 import ConsentConfirm from '@/components/service/profile/ProfileModal/ConsentConfirm';
 
 export default function ProfileModal() {
-  const [checked, setChecked] = useState(false);
+  const [selectedGender, setSelectedGender] = useState<'M' | 'F' | undefined>(undefined);
   const [selectedJob, setSelectedJob] = useState<Job | ''>('');
   const handleSubmit = () => {
     console.log('제출 완료!'); // 완료 버튼 클릭 시 처리할 작업
@@ -16,12 +17,13 @@ export default function ProfileModal() {
 
   return (
     <div className="p-4 space-y-3">
-
       {/* 헤더 */}
       <h2 className="text-lg font-semibold">나의 정보</h2>
       {/* 아이디 */}
       <p className="text-sm">이름: 홍길동</p>
+
       {/* 성별 */}
+      <GenderRadio selectedGender={selectedGender} setSelectedGender={setSelectedGender} />
 
       {/* 나이 */}
 
