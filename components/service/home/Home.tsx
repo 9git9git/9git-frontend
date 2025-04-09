e614e30570f42ce23e8811a9cf117de0a2877241
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-09 15:08:02 +0900

    main 화면/오늘의 ToDo/제작 완료

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -4,7 +4,8 @@ import { Header } from '@/components/shared/Header';
 import Card from '@/components/common/Card';
 import { ProgressBar } from '@/components/service/home/ProgressBar';
 
-import TodayTodo from './TodayToDo'; //
+import TodayTodo from './TodoPopup'; //
+import HomeContainer from './HomeContainer';
 
 export const Home = () => {
   return (
@@ -13,17 +14,7 @@ export const Home = () => {
         <Logo width={45} height={45} />
       </Header>
 
-      <div className="mt-6 flex flex-col items-center gap-4">
-        {/* 목표 진행률 카드 */}
-        <Card title="목표 진행률" isMore>
-          <ProgressBar value={75} title="영어" />
-        </Card>
-
-        {/* 오늘의 할 일 카드 */}
-        <Card title="오늘의 To-Do">
-          <TodayTodo />
-        </Card>
-      </div>
+      <HomeContainer />
     </div>
   );
 };
