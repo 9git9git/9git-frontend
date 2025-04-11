e763a5d5ebea3958420cacd2b2978157bd32a4a7
Eatcoding <trivialcoding6@gmail.com>
2025-04-11 11:42:41 +0900

    ✨ [SCRUM-120]-Mocking-환경-구성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import { MSWProvider } from '@/components/providers/MSWProvider';
 
 export const metadata: Metadata = {
-  title: "구깃",
-  description: "필요한 순간만 접어놓다, 내 하루의 구깃",
+  title: '구깃',
+  description: '필요한 순간만 접어놓다, 내 하루의 구깃',
 };
 
 export default function RootLayout({
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <MSWProvider>{children}</MSWProvider>
+      </body>
     </html>
   );
 }
